c595030e2fd2dc0692f72103e84e3816068fe387
SimSonic <simsonic@yandex.ru>
2015-07-03 18:59:56 +0600

    .

--- src/main/java/ru/methuselah/serversideplugin/API/ServerSideAPI.java
+++ src/main/java/ru/methuselah/serversideplugin/API/ServerSideAPI.java
@@ -25,6 +25,7 @@ public abstract class ServerSideAPI
 	 * @param uuid уникальный идентификатор игрока
 	 * @return Объект, содержащий методы получения информации об игроке
 	 * @throws ru.methuselah.authlib.methods.ResponseException при возникновении сетевых и иных проблем
+	 * @throws UnsupportedOperationException если веб-сервер не поддерживает данную операцию
 	 */
-	public abstract PlayerInformation getPlayerInfo(UUID uuid) throws ResponseException;
+	public abstract PlayerInformation getPlayerInfo(UUID uuid) throws ResponseException, UnsupportedOperationException;
 }
